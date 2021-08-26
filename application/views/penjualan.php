c578bb28b1d79528012f696bfe55790b72e155c7
ayung21 <ayungyung20@gmail.com>
2021-08-26 23:24:01 +0700

    view dashboard lokasi herbal

---
+++ application/views/penjualan.php
@@ -0,0 +1,55 @@
+<section class="content">
+    <row>
+        <div class="center_content">
+            <!-- <div class="center_title_bar">Latest Products</div> -->
+            <div class="prod_box">
+                <div class="center_prod_box">
+                    <div class="product_title"><a href="#">Makita 156 MX-VL</a></div>
+                    <div class="product_img"><a href="#"><img class="img_barang_herbal" src="<?= base_url(); ?>assets/dist/img/p1.jpg" alt="" border="0" /></a></div>
+                    <div class="prod_price"><span class="reduce">Rp 140.000 </span> <span class="price">Rp 120.000</span></div>
+                </div>
+                <div class="prod_details_tab"> <a href="#" class="prod_buy">Add to Cart</a> <a href="#" class="prod_details">Details</a> </div>
+            </div>
+            <div class="prod_box">
+                <div class="center_prod_box">
+                    <div class="product_title"><a href="#">Bosch XC</a></div>
+                    <div class="product_img"><a href="#"><img class="img_barang_herbal" src="<?= base_url(); ?>assets/dist/img/p2.jpg" alt="" border="0" /></a></div>
+                    <div class="prod_price"><span class="reduce"></span> <span class="price">Rp 120.000</span></div>
+                </div>
+                <div class="prod_details_tab"> <a href="#" class="prod_buy">Add to Cart</a> <a href="#" class="prod_details">Details</a> </div>
+            </div>
+            <div class="prod_box">
+                <div class="center_prod_box">
+                    <div class="product_title"><a href="#">Lotus PP4</a></div>
+                    <div class="product_img"><a href="#"><img class="img_barang_herbal" src="<?= base_url(); ?>assets/dist/img/p4.jpg" alt="" border="0" /></a></div>
+                    <div class="prod_price"><span class="reduce"></span> <span class="price">Rp 120.000</span></div>
+                </div>
+                <div class="prod_details_tab"> <a href="#" class="prod_buy">Add to Cart</a> <a href="#" class="prod_details">Details</a> </div>
+            </div>
+            <div class="prod_box">
+                <div class="center_prod_box">
+                    <div class="product_title"><a href="#">Makita 156 MX-VL</a></div>
+                    <div class="product_img"><a href="#"><img class="img_barang_herbal" src="<?= base_url(); ?>assets/dist/img/p3.jpg" alt="" border="0" /></a></div>
+                    <div class="prod_price"><span class="reduce"></span> <span class="price">Rp 120.000</span></div>
+                </div>
+                <div class="prod_details_tab"> <a href="#" class="prod_buy">Add to Cart</a> <a href="#" class="prod_details">Details</a> </div>
+            </div>
+            <div class="prod_box">
+                <div class="center_prod_box">
+                    <div class="product_title"><a href="#">Bosch XC</a></div>
+                    <div class="product_img"><a href="#"><img class="img_barang_herbal" src="<?= base_url(); ?>assets/dist/img/p5.jpg" alt="" border="0" /></a></div>
+                    <div class="prod_price"><span class="reduce"></span> <span class="price">Rp 120.000</span></div>
+                </div>
+                <div class="prod_details_tab"> <a href="#" class="prod_buy">Add to Cart</a> <a href="#" class="prod_details">Details</a> </div>
+            </div>
+            <div class="prod_box">
+                <div class="center_prod_box">
+                    <div class="product_title"><a href="#">Lotus PP4</a></div>
+                    <div class="product_img"><a href="#"><img class="img_barang_herbal" src="<?= base_url(); ?>assets/dist/img/p6.jpg" alt="" border="0" /></a></div>
+                    <div class="prod_price"><span class="reduce"></span> <span class="price">Rp 120.000</span></div>
+                </div>
+                <div class="prod_details_tab"> <a href="#" class="prod_buy">Add to Cart</a> <a href="#" class="prod_details">Details</a> </div>
+            </div>
+        </div>
+    </row>
+</section>
