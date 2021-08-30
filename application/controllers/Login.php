10d9854bbc8bf9826ee571306f49b17d58dbc86a
ayung21 <ayungyung20@gmail.com>
2021-08-31 06:08:12 +0700

    form data diri toko

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -12,3 +12,13 @@ class Login extends CI_Controller {
 	{
 		$this->load->view('daftar');
 	}
+
+	public function formDataDiriToko()
+	{
+		$data = array(
+			'content'	=> 'form_datadiri_toko',
+		);
+		$this->load->view('components/main', $data);
+	}
+
+}
---
+++ application/views/form_datadiri_toko.php
@@ -0,0 +1,56 @@
+<!-- Main content -->
+<section class="content">
+    <div class="row">
+        <div class="col-md-12">
+            <!-- general form elements -->
+            <div class="box box-primary">
+                <div class="box-header">
+                    <h3 class="box-title">Quick Example</h3>
+                </div>
+                <!-- /.box-header -->
+                <!-- form start -->
+                <form role="form">
+                    <div class="box-body">
+                        <div class="file-field">
+                            <div class="mb-4">
+                                <img src="https://mdbootstrap.com/img/Photos/Others/placeholder-avatar.jpg" class="rounded-circle z-depth-1-half avatar-pic" alt="example placeholder avatar">
+                            </div>
+                            <div class="d-flex justify-content-center">
+                                <div class="btn btn-mdb-color btn-rounded float-left">
+                                    <span>Add photo</span>
+                                    <input type="file" class="input-form-file">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email" />
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Password</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputFile">File input</label>
+                            <input type="file" id="exampleInputFile" />
+                            <p class="help-block">
+                                Example block-level help text here.
+                            </p>
+                        </div>
+                        <div class="checkbox">
+                            <label> <input type="checkbox" /> Check me out </label>
+                        </div>
+                    </div>
+                    <!-- /.box-body -->
+
+                    <div class="box-footer">
+                        <button type="submit" class="btn btn-primary">
+                            Submit
+                        </button>
+                    </div>
+                </form>
+            </div>
+            <!-- /.box -->
+        </div>
+    </div>
+</section>
