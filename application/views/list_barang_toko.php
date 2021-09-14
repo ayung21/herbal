6904700cc0aeeb122290c16d4d485fae94d79ba3
ayung21 <ayungyung20@gmail.com>
2021-09-14 23:42:41 +0700

    button tambah barang toko

--- application/views/list_barang_toko.php
+++ application/views/list_barang_toko.php
@@ -6,6 +6,9 @@
             <div class="box">
                 <div class="box-header">
                     <h3 class="box-title">List Barang Toko</h3>
+                    <div class="pull-right box-tools">
+                        <button class="btn btn-info btn-sm" title="Remove" data-toggle="modal" data-target="#myModal">Tambah Kota</button>
+                    </div>
                 </div><!-- /.box-header -->
                 <div class="box-body">
                     <table id="example1" class="table table-bordered table-striped">
@@ -45,3 +48,50 @@
         </div><!-- /.col -->
     </div><!-- /.row -->
 </section><!-- /.content -->
+
+<!-- Modal -->
+<div class="" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <?php echo form_open('Home/createBarang') ?>
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Create Barang</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="box-body">
+                    <div class="file-field">
+                        <div class="mb-4">
+                            <img src="https://mdbootstrap.com/img/Photos/Others/placeholder-avatar.jpg" class="rounded-circle z-depth-1-half avatar-pic2" alt="example placeholder avatar">
+                        </div>
+                        <div class="d-flex justify-content-center">
+                            <div class="btn btn-mdb-color btn-rounded float-left">
+                                <span>Add photo</span>
+                                <input type="file" class="input-form-file">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label>Nama Barang</label>
+                        <input type="text" name="Harga" class="form-control" placeholder="Harga" required />
+                    </div>
+                    <div class="form-group">
+                        <label>Harga</label>
+                        <input type="text" name="Harga" class="form-control" placeholder="Harga" required />
+                    </div>
+                    <div class="form-group">
+                        <label>Diskon</label>
+                        <input type="text" name="Harga" class="form-control" placeholder="Harga" required />
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Save changes</button>
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
