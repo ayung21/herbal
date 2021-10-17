e163320f825616deb49912c69588fee3750d9df4
ayung21 <ayungyung20@gmail.com>
2021-10-17 22:33:58 +0700

    configuration

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url','file','form','user','string');
+$autoload['helper'] = array('url','file','form','user','string','auth');
 
 /*
 | -------------------------------------------------------------------
