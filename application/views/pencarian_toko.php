0692498d17c1c9654cdfb8eae161c163e39904fc
ayung21 <ayungyung20@gmail.com>
2021-11-07 17:30:39 +0700

    tester

--- application/views/pencarian_toko.php
+++ application/views/pencarian_toko.php
@@ -43,13 +43,12 @@
 </section><!-- /.content -->
 <script>
     function myFunction() {
-        alert();
-        // if (navigator.geolocation) {
-		// 	navigator.geolocation.getCurrentPosition(showPosition);
-		// } else {
-		// 	//   x.innerHTML = "Geolocation is not supported by this browser.";
-		// 	$("div.infor").text("Geolocation is not supported by this browser.");
-		// }
+        if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(showPosition);
+		} else {
+			//   x.innerHTML = "Geolocation is not supported by this browser.";
+			$("div.infor").text("Geolocation is not supported by this browser.");
+		}
     }
     function showPosition(position) {
 		$.ajax({
