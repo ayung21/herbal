51528e607bc86c1df963cf21c9016817aa2c5996
ayung21 <ayungyung20@gmail.com>
2022-01-12 18:53:13 +0700

    detail penjualan

--- application/views/detail_penjualan.php
+++ application/views/detail_penjualan.php
@@ -1,22 +1,22 @@
 <section>
     <row>
         <div class="center_content">
-            <div class="center_title_bar" style="background-color: red;">Makita 156 MX-VL</div>
+            <div class="center_title_bar" style="background-color: red;"><?= $data->nama_barang; ?></div>
             <div class="prod_box_big">
                 <div class="center_prod_box_big" style="margin-left: -20px;">
                     <div class="product_img_big">
-                        <div class="thumbs"><img src="<?= base_url("uploads/img/tempN38v745gxCqRYoPr-2021-11-30_20:11:28.jpg"); ?>" style="width: 200px;margin-left: 6px;margin-top: 10px;" alt="" border="0" /></div>
+                        <div class="thumbs"><img src="<?= !empty($data->image_name) ? base_url('uploads/img/' . $data->image_name) : 'https://mdbootstrap.com/img/Photos/Others/placeholder-avatar.jpg'; ?>" style="width: 200px;margin-left: 6px;margin-top: 10px;" alt="" border="0" /></div>
                     </div>
-                    <div class="details_big_box">
-                        <div class="product_title_big">Makita 156 MX-VL</div>
-                        <div class="specifications"> Available: <span class="blue">In stock</span><br />
+                    <div class="details_big_box" style="margin-top: 20px;">
+                        <div class="product_title_big"><?= $data->nama_barang; ?></div>
+                        <!-- <div class="specifications"> Available: <span class="blue">In stock</span><br />
                             Warranties: <span class="blue">24 months</span><br />
                             Transport: <span class="blue"> delivery services company</span><br />
                             Include :<span class="blue"> TVA</span><br />
                             Description :<span class="blue"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </span><br />
-                        </div>
-                        <div class="prod_price_big"><span class="reduce">350$</span> <span class="price">270$</span></div>
-                        <a href="#" class="prod_buy">add to cart</a> <a href="#" class="prod_compare">compare</a>
+                        </div> -->
+                        <div class="prod_price_big"><span class="price">Rp. <?= $data->harga; ?></span></div>
+                        <!-- <a href="#" class="prod_buy">add to cart</a> <a href="#" class="prod_compare">compare</a> -->
                     </div>
                 </div>
             </div>
