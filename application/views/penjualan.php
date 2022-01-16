a4ce2cb194f67a9d9d4fbe54d04bc38678f07eeb
ayung21 <ayungyung20@gmail.com>
2022-01-16 22:47:38 +0700

    perbaikan ukurran gambar

--- application/views/penjualan.php
+++ application/views/penjualan.php
@@ -5,7 +5,7 @@
                 <div class="prod_box_mobile">
                     <div class="center_prod_box_mobile">
                         <div class="product_title_mobile"><a href="#"><?= $row->nama_barang ?></a></div>
-                        <div class="product_img_mobile"><a href="#"><img class="img_barang_herbal_mobile" src="<?= !empty($row->image_name) ? base_url('uploads/img/' . $row->image_name) : 'https://mdbootstrap.com/img/Photos/Others/placeholder-avatar.jpg'; ?>" alt="" border="0" height="215px" /></a></div>
+                        <div class="product_img_mobile"><a href="#"><img class="img_barang_herbal_mobile" src="<?= !empty($row->image_name) ? base_url('uploads/img/' . $row->image_name) : 'https://mdbootstrap.com/img/Photos/Others/placeholder-avatar.jpg'; ?>" alt="" border="0" height="215px" style="height: 25px;" /></a></div>
                         <div class="prod_price"><span class="reduce"></span> <span class="price">Rp <?= $row->harga ?></span></div>
                     </div>
                     <div class="prod_details_tab">
@@ -17,7 +17,7 @@
                 <div class="prod_box">
                     <div class="center_prod_box">
                         <div class="product_title"><a href="#"><?= $row->nama_barang ?></a></div>
-                        <div class="product_img"><a href="#"><img class="img_barang_herbal" src="<?= !empty($row->image_name) ? base_url('uploads/img/' . $row->image_name) : 'https://mdbootstrap.com/img/Photos/Others/placeholder-avatar.jpg'; ?>" alt="" border="0" height="215px" /></a></div>
+                        <div class="product_img"><a href="#"><img class="img_barang_herbal" src="<?= !empty($row->image_name) ? base_url('uploads/img/' . $row->image_name) : 'https://mdbootstrap.com/img/Photos/Others/placeholder-avatar.jpg'; ?>" alt="" border="0" height="215px" style="height: 25px;" /></a></div>
                         <div class="prod_price"><span class="reduce"></span> <span class="price">Rp <?= $row->harga ?></span></div>
                     </div>
                     <div class="prod_details_tab">
