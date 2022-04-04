50d23509e78e6c47b375daf487fb8c4c2d3e875e
ayung21 <ayungyung20@gmail.com>
2022-04-05 04:16:21 +0700

    perbaikan position detail

--- application/views/detail_penjualan.php
+++ application/views/detail_penjualan.php
@@ -2,10 +2,13 @@
     <row>
         <div class="center_content">
             <div class="center_title_bar" style="background-color: red;"><?= $data->nama_barang; ?></div>
+            <br>
+            <br>
             <div class="prod_box_big">
                 <div class="center_prod_box_big" style="margin-left: -20px;">
                     <div class="product_img_big">
-                        <div class="thumbs"><img src="<?= !empty($data->image_name) ? base_url('uploads/img/' . $data->image_name) : 'https://mdbootstrap.com/img/Photos/Others/placeholder-avatar.jpg'; ?>" style="width: 200px;margin-left: 6px;margin-top: 10px;" alt="" border="0" /></div>
+                        <?php if(parse_url($data->image_name, PHP_URL_SCHEME) == 'https' || parse_url($data->image_name, PHP_URL_SCHEME) == 'http'){ $image = $data->image_name;}else{ $image = base_url('uploads/img/' . $data->image_name);} ?>
+                        <div class="thumbs"><img src="<?= !empty($data->image_name) ? $image : 'https://mdbootstrap.com/img/Photos/Others/placeholder-avatar.jpg'; ?>" style="width: 200px;margin-left: 6px;margin-top: 10px;" alt="" border="0" /></div>
                     </div>
                     <div class="details_big_box" style="margin-top: 20px;">
                         <div class="product_title_big"><?= $data->nama_barang; ?></div>
