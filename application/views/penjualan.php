419cf50a3d1276dc378d24599b730a039dbd153b
ayung21 <ayungyung20@gmail.com>
2022-04-05 04:08:56 +0700

    number format harga

--- application/views/penjualan.php
+++ application/views/penjualan.php
@@ -7,7 +7,7 @@
                     <div class="center_prod_box_mobile">
                         <div class="product_title_mobile"><a href="#"><?= $row->nama_barang ?></a></div>
                         <div class="product_img_mobile"><a href="<?= base_url('Home/detail_penjualan/' . $row->id_transaksi); ?>"><img class="img_barang_herbal_mobile" src="<?= !empty($row->image_name) ? $image : 'https://mdbootstrap.com/img/Photos/Others/placeholder-avatar.jpg'; ?>" alt="" border="0" height="215px" style="height: 25px;" /></a></div>
-                        <div class="prod_price"><span class="reduce"></span> <span class="price">Rp <?= $row->harga ?></span></div>
+                        <div class="prod_price"><span class="reduce"></span> <span class="price">Rp <?= number_format($row->harga,0,',','.') ?></span></div>
                     </div>
                     <div class="prod_details_tab">
                         <!-- <div class="prod_price"><span class="reduce"></span> <?= $row->nama_toko ?></div> -->
@@ -20,7 +20,7 @@
                     <div class="center_prod_box">
                         <div class="product_title"><a href="#"><?= $row->nama_barang ?></a></div>
                         <div class="product_img"><a href="<?= base_url('Home/detail_penjualan/' . $row->id_transaksi); ?>"><img class="img_barang_herbal" src="<?= !empty($row->image_name) ? $image : 'https://mdbootstrap.com/img/Photos/Others/placeholder-avatar.jpg'; ?>" alt="" border="0" height="215px" style="height: 215px;" /></a></div>
-                        <div class="prod_price"><span class="reduce"></span> <span class="price">Rp <?= $row->harga ?></span></div>
+                        <div class="prod_price"><span class="reduce"></span> <span class="price">Rp <?= number_format($row->harga,0,',','.') ?></span></div>
                     </div>
                     <div class="prod_details_tab">
                         <!-- <div class="prod_price"><span class="reduce"></span> <?= $row->nama_toko ?></div> -->
