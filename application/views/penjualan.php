13abcf3722aef0a9e9b1febd4c648ec34e97732d
ayung21 <ayungyung20@gmail.com>
2022-04-09 11:57:58 +0700

    pagination

--- application/views/penjualan.php
+++ application/views/penjualan.php
@@ -1,13 +1,17 @@
 <section class="content" style="height: <?= $height; ?>;">
     <div class="center_content">
         <?php foreach ($data as $row) : ?>
-            <?php if(parse_url($row->image_name, PHP_URL_SCHEME) == 'https' || parse_url($row->image_name, PHP_URL_SCHEME) == 'http'){ $image = $row->image_name;}else{ $image = base_url('uploads/img/' . $row->image_name);} ?>
+            <?php if (parse_url($row->image_name, PHP_URL_SCHEME) == 'https' || parse_url($row->image_name, PHP_URL_SCHEME) == 'http') {
+                $image = $row->image_name;
+            } else {
+                $image = base_url('uploads/img/' . $row->image_name);
+            } ?>
             <?php if (mobile()) : ?>
                 <div class="prod_box_mobile">
                     <div class="center_prod_box_mobile">
                         <div class="product_title_mobile"><a href="#"><?= $row->nama_barang ?></a></div>
                         <div class="product_img_mobile"><a href="<?= base_url('Home/detail_penjualan/' . $row->id_transaksi); ?>"><img class="img_barang_herbal_mobile" src="<?= !empty($row->image_name) ? $image : 'https://mdbootstrap.com/img/Photos/Others/placeholder-avatar.jpg'; ?>" alt="" border="0" height="215px" style="height: 25px;" /></a></div>
-                        <div class="prod_price"><span class="reduce"></span> <span class="price">Rp <?= number_format($row->harga,0,',','.') ?></span></div>
+                        <div class="prod_price"><span class="reduce"></span> <span class="price">Rp <?= number_format($row->harga, 0, ',', '.') ?></span></div>
                     </div>
                     <div class="prod_details_tab">
                         <!-- <div class="prod_price"><span class="reduce"></span> <?= $row->nama_toko ?></div> -->
@@ -20,7 +24,7 @@
                     <div class="center_prod_box">
                         <div class="product_title"><a href="#"><?= $row->nama_barang ?></a></div>
                         <div class="product_img"><a href="<?= base_url('Home/detail_penjualan/' . $row->id_transaksi); ?>"><img class="img_barang_herbal" src="<?= !empty($row->image_name) ? $image : 'https://mdbootstrap.com/img/Photos/Others/placeholder-avatar.jpg'; ?>" alt="" border="0" height="215px" style="height: 215px;" /></a></div>
-                        <div class="prod_price"><span class="reduce"></span> <span class="price">Rp <?= number_format($row->harga,0,',','.') ?></span></div>
+                        <div class="prod_price"><span class="reduce"></span> <span class="price">Rp <?= number_format($row->harga, 0, ',', '.') ?></span></div>
                     </div>
                     <div class="prod_details_tab">
                         <!-- <div class="prod_price"><span class="reduce"></span> <?= $row->nama_toko ?></div> -->
@@ -30,6 +34,12 @@
                 </div>
             <?php endif; ?>
         <?php endforeach; ?>
+        <div class="row">
+            <div class="col">
+                <!--Tampilkan pagination-->
+                <?= $pagination; ?>
+            </div>
+        </div>
         <!-- <div class="center_title_bar">Latest Products</div> -->
         <!-- <div class="prod_box">
                 <div class="center_prod_box">
